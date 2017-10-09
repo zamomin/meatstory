5a227982abea72d50b8279e13c3ec0f25ee6e8ee
Marlen Buri <ch-murmeli@gmx.ch>
2017-10-09 17:53:48 +0200

    first presentable version

--- src/App.js
+++ src/App.js
@@ -9,111 +9,149 @@ import {
   Image,
   Input,
   Form,
+  Radio,
 } from 'semantic-ui-react';
 
 const App = () => (
   <Container>
     <div>
-      <Image src='/img/logo-meatstory@2x.png' size= 'medium' centered className='logoMeatstory'/>
+      <Segment basic>
+        <Image src='/img/logo-meatstory@2x.png' size='small'/>
+      </Segment>
     </div>
-    <Segment basic secondary>
-      <Header as='h2'>Wer liegt auf deinem Teller?</Header>
+    <div className='grey'>
+      <Segment basic>
+      <Header size='huge'>Was liegt auf deinem Teller?</Header>
       Wir machen die Geschichte deines Steaks sichtbar.
-    </Segment>
-    <Segment basic>
-      <Header as='h3'>So gehts los:</Header>
-      <Feed>
-        <Feed.Event>
-          <Feed.Label>
-            <Header as='h2'>1</Header>
-          </Feed.Label>
+      </Segment>
+    </div>
+    <div>
+      <Segment basic>
+        <Header as='h2'>So gehts los:</Header>
+        <Feed>
+          <Feed.Event>
+            <Feed.Label>
+              <Header as='h2'>1</Header>
+            </Feed.Label>
           <Feed.Content>
-            Kauf dir ein Stück Fleisch
+            Kauf dir ein Stück Fleisch. MeatStory ermöglicht Dir, die genaue Herkunft von einem Stück Fleisch zurückzuverfolgen. Händler und Vermarkter können mit MeatStory eine viel geschätzte Transparenz bieten.
           </Feed.Content>
-        </Feed.Event>
-        <Feed.Event>
-          <Feed.Label>
-            <Header as='h2'>2</Header>
-          </Feed.Label>
+          </Feed.Event>
+          <Feed.Event>
+            <Feed.Label>
+              <Header as='h2'>2</Header>
+            </Feed.Label>
           <Feed.Content>
-            Scan den QR Code <br/> oder <br/> gib den Code hier ein
+            Scanne den QR Code auf dem Produkt<br/> oder <br/> gibt die MeatStory-Nummer manuell ein. Die Nummer entspricht der Tierverkehrsdatenbank-Nummer, Beispiel CH120.1166.0014.6. Die Eingabe erfolgt ohne CH und ohne Punkte: 120116600146.
           </Feed.Content>
-        </Feed.Event>
-        <Feed.Event>
-          <Feed.Label>
-            <Header as='h2'>3</Header>
-          </Feed.Label>
-          <Feed.Content>
-            Lies die Story und geniesse deine Mahlzeit
-          </Feed.Content>
-        </Feed.Event>
-      </Feed>
-    </Segment>
-    <Segment basic secondary>
-      <Header as='h2'>Über uns</Header>
-      Mit der "Meatstory" möchten wir den Weg des Fleisches von der Weide bis auf den Teller transparent machen.
-    </Segment>
-    <Segment basic>
-      <Header as='h2'>Let us know your thoughts!</Header>
-      <Form>
-        <Form.Group grouped>
-        <label><Header as='h3'>Was ist die interessanteste Information?</Header></label>
-        <Form.Field label='Der Geburtsort' control='input' type='radio' name='htmlRadios' />
-        <Form.Field label='Das Alter' control='input' type='radio' name='htmlRadios' />
-        <Form.Field label='Das Gewicht' control='input' type='radio' name='htmlRadios' />
-        </Form.Group>
-      </Form>
-      <Form>
-        <Form.Group grouped>
-        <label><Header as='h3'>Was schmeckt am besten?</Header></label>
-        <Form.Field label='Braten' control='input' type='radio' name='htmlRadios' />
-        <Form.Field label='Wurst' control='input' type='radio' name='htmlRadios' />
-        <Form.Field label='Steak' control='input' type='radio' name='htmlRadios' />
-        </Form.Group>
-      </Form>
-    </Segment>
-    <Segment basic inverted color='grey'>
-      <Header as='h2'>Partner</Header>
-      <div className='is-flex'>
-        <img src='/img/BFH+HAFL@2x.png' className='bfh'/>
-        <img src='/img/Identitas@2x.png'/>
-        <img src='/img/OpenData.ch@2x.png'/>
-        <img src='/img/OpenFoodlogo@2x.png'/>
-        <img src='/img/20min@2x.png'/>
-        <img src='/img/SonntagsZeitung@2x.png'/>
-      </div>
-    </Segment>
-    <Segment basic inverted>
-      <Header as='h2'>Get in touch</Header>
-      sam@meatstory.ch
-    </Segment>
-    <Segment basic secondary>
-      TDV Nummer auf der Etikette <br/>
-      <Input action='GO' />
-    </Segment>
-    <Segment basic>
-      <Header as='h2'>Frida la vache qui rit</Header>
-      Murten, Fr <br/>
-      Irish Golden Hopper
-      <Feed>
-        <Feed.Event>
-          <Feed.Label>
-            <img src='/img/cow.svg' size= 'tiny' />
-          </Feed.Label>
-          <Feed.Content>
-            Die Irish Golden Hopper Frida war die Tochter von Samira und Elron und ist am 1.April 2016 zur Welt gekommen mit einem Gewicht von 48 Kilogramm. <br/>Dank der zarten Gräser und Kräuter ist ihr Fleisch saftig und schmackhaft geworden.
-          </Feed.Content>
-        </Feed.Event>
-        <Feed.Event>
-          <Feed.Label>
-            <img src='/img/tractor.svg' size= 'tiny' />
-          </Feed.Label>
-          <Feed.Content>
-            Nach der Geburt bei der Familie Hornbläser in Finkenschlupf ging die Reise nach Weissfurchen. <br/>Dort verbrachte sie viel Zeit fressend und wiederkäuend mit ihren FreundInnen auf den Weiden.
-          </Feed.Content>
-        </Feed.Event>
-      </Feed>
-    </Segment>
+          </Feed.Event>
+          <Feed.Event>
+            <Feed.Label>
+              <Header as='h2'>3</Header>
+            </Feed.Label>
+            <Feed.Content>
+             Informiere dich über die Herkunft deines Produkts, lerne seine Geschichte kennen und geniesse deine Mahlzeit.
+            </Feed.Content>
+          </Feed.Event>
+        </Feed>
+      </Segment>
+    </div>
+    <div className='grey'>
+      <Segment basic>
+        <Header as='h2'>Über uns</Header>
+        <p>MeatStory ermöglicht Konsumenten die Herkunft von Fleischprodukten zu verfolgen. Die transparente Rückverfolgbarkeit erleichtert ihnen informierte Entscheidungen zu treffen.</p>
+        <p>Samuel Bühlmann schafft begeistert mehr Bewusstsein für dein Plätzli à la Minute.</p>
+        <p>Simon Josi entwickelt geniale Rückverfolgbarkeit von deinem Teller zur Wiese.</p>
+        <p>Pascal Geronimi zeichnet gekonnt Daten für Lebensmittel-LliebhaberInnen auf.</p>
+      </Segment>
+    </div>
+    <div>
+      <Segment basic>
+        <Header as='h2'>Teilen Sie uns Ihre Meinung mit!</Header>
+        <Form>
+          <Form.Group grouped>
+            <Header as='h3'>Wie finden Sie Ihre MeatStory</Header>
+            <Form.Field>
+              <Radio
+                label='Interessant'
+                name='radioGroup'
+                value='interessant'
+              />
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                label='Durchschnittlich'
+                name='radioGroup'
+                value='durchschnittlich'
+              />
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                label='Uninteressant'
+                name='radioGroup'
+                value='uniteressant'
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group grouped>
+            <Header as='h3'>Welche Information finden Sie besonders interessant, was vermissen Sie?</Header>
+            <Form.Field>
+              <Input action='GO'/>
+            </Form.Field>
+          </Form.Group>
+        </Form>
+      </Segment>
+    </div>
+    <div>
+      <Segment basic inverted color='grey'>
+        <Header as='h2'>Partner</Header>
+        <div className='is-flex partner'>
+          <img src='/img/OpenData.ch@2x.png'/>
+          <img src='/img/OpenFoodlogo@2x.png'/>
+          <Image src='/img/id.png'/>
+          <Image src='/img/BFH1.png'/>
+          <Image src='/img/20min@2x.png'/>
+          <Image src='/img/SonntagsZeitung@2x.png'/>
+        </div>
+      </Segment>
+    </div>
+    <div>
+      <Segment basic inverted>
+        <Header as='h2'>Get in touch</Header>
+        <p>sam@meatstory.ch</p>
+      </Segment>
+    </div>
+    <div>
+      <Segment basic tertiary>
+        <p>TDV Nummer auf der Etikette</p>
+        <Input action='GO' />
+      </Segment>
+    </div>
+    <div>
+      <Segment basic>
+        <Header as='h2'>Frida la vache qui rit</Header>
+        Murten, Fr <br/>
+        Irish Golden Hopper
+        <Feed>
+          <Feed.Event>
+            <Feed.Label>
+              <img src='/img/cow.svg' size= 'tiny' className='icons'/>
+            </Feed.Label>
+            <Feed.Content>
+              Die Irish Golden Hopper Frida war die Tochter von Samira und Elron und ist am 1.April 2016 zur Welt gekommen mit einem Gewicht von 48 Kilogramm. <br/>Dank der zarten Gräser und Kräuter ist ihr Fleisch saftig und schmackhaft geworden.
+            </Feed.Content>
+          </Feed.Event>
+          <Feed.Event>
+            <Feed.Label>
+              <img src='/img/tractor.svg' size= 'tiny' className='icons'/>
+            </Feed.Label>
+            <Feed.Content>
+              Nach der Geburt bei der Familie Hornbläser in Finkenschlupf ging die Reise nach Weissfurchen. <br/>Dort verbrachte sie viel Zeit fressend und wiederkäuend mit ihren FreundInnen auf den Weiden.
+            </Feed.Content>
+          </Feed.Event>
+        </Feed>
+      </Segment>
+    </div>
   </Container>
 )
 
